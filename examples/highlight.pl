87956c855907760ffa2624545eda39d23bfb13ab
Nadim Khemir <nadim.khemir@gmail.com>
2017-03-26 09:51:41 +0200

    ADDED: simple highlight example

---
+++ examples/highlight.pl
@@ -0,0 +1,34 @@
+
+use Data::Dump::Tree ;
+use Data::Dump::Tree::Enums ;
+
+use Terminal::ANSIColor ;
+
+# -------------------------------------------------
+# highlight some entries via a  user defined filter
+# -------------------------------------------------
+
+
+# the data structure to dump
+
+class Tomatoe{}
+class Potatoe{}
+
+my $s = [1..4] ;
+
+# dump filtered
+dump $s, header_filters => (&my_filter,) ;
+
+
+# HEADER FILTER
+multi sub my_filter(\r, Int $s, ($depth, $path, $glyph, @renderings), (\k, \b, \v, \f, \final, \want_address))
+{
+if $s == 1 { @renderings.append: $glyph ~ color('bold white on_yellow') ~ ' add line in the graph ' }
+
+if $s == 2 { k = k ~ color('bold white on_yellow') ~ '*' }
+
+if $s == 3 { k = color('bold white on_yellow') ~ k }
+
+if $s == 4 { f = f ~ ' ' ~ color('bold white on_yellow') ~ 'an Int' }
+}
+
