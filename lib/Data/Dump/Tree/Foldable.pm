a6ad7ad9619ff0fb8eba6679ad7db4335a1a803b
Nadim Khemir <nadim.khemir@gmail.com>
2017-07-05 15:15:25 +0200

    ADDED: Foldable

---
+++ lib/Data/Dump/Tree/Foldable.pm
@@ -0,0 +1,285 @@
+
+=begin pod
+
+=NAME Data::Dump::Tree::Foldable 
+
+=SYNOPSIS
+
+	use Data::Dump::Tree::Foldable ;
+
+	my $f = Data::Dump::Tree::Foldable.new: $data_to_dump, :title<title> ;
+	my $v = $f.get_view ; # default geometry
+
+	$v.set: :top_line<1>, :page_size<10> ; # set geometry
+	#get the lines to display
+	my @lines = $v.get_lines ;
+	 
+	# move in the data structure rendering
+	$v.line_down ;
+	$v.page_down ;
+	
+	# select a line and fold it, or unfold it if it is already folded
+	$v.set: :selected_line(7) ;
+	$v.fold_flip_selected ;
+	
+	#get the lines to display
+	@line = $v.get_lines ;
+
+=DESCRIPTION
+
+U<Data::Dump::Tree::Foldable> and U<Data::Dump::Tree::Foldable::View> implement
+the base mechanisms needed to:
+
+=item display a structured rendered by <Data::Dump::Tree> in a viewport
+
+=item movement through the data structure in the viewport
+ 
+=item folding the data structure.
+
+A simple search functionality is also planned in future versions.
+
+
+=head1 Data::Dump::Tree::Foldable
+
+	my $f = Data::Dump::Tree::Foldable.new: $data_to_dump, :title<title> ;
+
+
+A Foldable contains a U<Data:Dump::Tree> rendering. I takes the same arguments
+as U<Data:Dump::Tree>. 
+
+=head2 method get_view
+
+	my $v = $f.get_view ;
+
+Returns a view to the rendering. You can create multiple views from a single 
+Foldable object. The views share the Foldable but each view has its own folds
+and geometry.
+
+=head1 Data::Dump::Tree::Foldable::View
+
+A view renderer for a U<Data::Dump::Tree> rendering.
+
+You can set the geometry of the view then navigate the data structure and fold
+it too.
+
+=head2 method set(:$page_size, :$top_line, :$selected_line)
+
+Set the geometry of the view where:
+
+=item :page_size is the height of the view
+
+=item :top_line is the line of the rendering displayed at the top of the window
+
+=item :selected_line is a line index in the view
+
+You can call I<set> with a combination of named arguments, eg: you don't need
+to set all of them.
+
+=head2 method line_up()
+
+Scrolls the view up showing previous lines of the rendering
+
+=head2 method page_up()
+
+Scrolls the view up, the height of a view
+
+=head2 method line_down()
+
+Scrolls the view down showing following lines of the rendering
+
+=head2 method page_down()
+
+Scrolls the view down, the height of a view
+
+=head2 method fold_flip_selected()
+
+Folds or unfolds the data under the selected_line, see I<set>.
+
+=head2 method fold_all()
+
+Folds all the data 
+
+=head2 method unfold_all() 
+
+Unfolds all the data
+
+=head2 method get_lines()
+
+Return all the lines visible in the view, may be less than the view height and
+even zero lines in case of a view size equal to 0
+
+=AUTHOR
+
+Nadim ibn hamouda el Khemir
+https://github.com/nkh
+
+=LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl6 itself.
+
+=SEE-ALSO
+
+Data::Dump::Tree
+
+=end pod
+
+
+use Data::Dump::Tree ;
+
+enum (SKIP => 0, 'FOLDED', 'PARENT_FOLDED') ;
+
+class Data::Dump::Tree::Foldable::View {...}
+
+class Data::Dump::Tree::Foldable 
+{
+has @.lines ;
+has @.folds ;
+
+method new($s, *%attributes)
+{
+
+my $dumper = Data::Dump::Tree.new(|%attributes) ;
+
+my ($lines, $wrap_data) 
+	= $dumper.get_dump_lines(
+			$s, 
+			:wrap_header(&header_wrap),
+			:wrap_footer(&footer_wrap),
+			);
+
+self.bless: :lines(|$lines), :folds(|$wrap_data<folds>) ;
+}
+
+my sub header_wrap(
+	\wd,
+	($glyph, $continuation_glyph, $multi_line_glyph),
+	($kvf, @ks, @vs, @fs),
+	Mu $s,
+	($depth, $path, $filter_glyph, @renderings),
+	($k, $b, $v, $f, $final, $want_address),
+	) 
+{
+wd<folds>.push: @renderings.elems ;
+wd<folds>.elems - 1 ; # token passed to footer callback
+}
+
+my sub footer_wrap(\wd, Mu $s, $final, ($depth, $filter_glyph, @renderings), $header_wrap_token)
+{
+wd<folds>[$header_wrap_token] [R-]= @renderings.elems ;
+}
+
+method get_view(*%options)
+{
+Data::Dump::Tree::Foldable::View.new:
+	:foldable(self),
+	:folds($.folds.map: { [$_, 0, 0] }),
+	|%options,
+}
+
+
+} # class
+
+
+class Data::Dump::Tree::Foldable::View 
+{
+has Data::Dump::Tree::Foldable $.foldable ;
+
+has Int $.top_line = 0 ;
+has Int $.page_size = 25 ;
+has Int $.selected_line = 0 ;
+has @.folds ;
+
+has Bool $.search_folds = True ;
+
+method set(:$page_size, :$top_line, :$selected_line)
+{ 
+$page_size andthen $!page_size = max $page_size, 0 ;
+
+$!top_line = max(min($top_line, @!folds - $!page_size), 0) with $top_line ;
+
+with $selected_line 
+	{
+	my $current_line = $!top_line ;
+
+	for ^max(min($selected_line, $!page_size), 0) 
+		{
+		$current_line += @!folds[$_][FOLDED] 
+					?? @!folds[$_][SKIP] + 1
+					!! 1 ;
+		}
+
+	$!selected_line = $current_line ;
+	}
+}
+
+method line_up()
+{
+$!top_line-- ;
+
+while @!folds[$!top_line][PARENT_FOLDED]
+	{ $!top_line-- }
+
+$!top_line max= 0 ;
+}
+
+method page_up() { $.line_up for ^$!page_size }
+
+method line_down()
+{
+$!top_line++ ;
+  
+while @!folds[$!top_line][PARENT_FOLDED]
+	{ $!top_line++ }
+
+$!top_line min= @!folds - 1 ;
+}
+
+method page_down() { $.line_down for ^$!page_size }
+
+method fold_flip_selected()
+{ 
+my $state = @!folds[$!selected_line][FOLDED] +^= 1 ;
+
+my @sub_elements = @!folds[ ($!selected_line + 1) .. ($!selected_line + @!folds[$!selected_line][SKIP]) ] ;
+
+while @sub_elements
+	{
+ 	$_ = @sub_elements.shift ;
+
+	if $_[FOLDED]
+		{
+		# skip this fold
+		@sub_elements.shift for $_[SKIP] ;	
+		}
+	else
+		{
+		$_[PARENT_FOLDED] = $state ;
+		}
+	}
+}
+
+method fold_all()   { for @!folds { $_[FOLDED] = $_[PARENT_FOLDED] = 1 } }
+method unfold_all() { for @!folds { $_[FOLDED] = $_[PARENT_FOLDED] = 0 } }
+
+method get_lines()
+{
+my ($current_line, @lines) = ($!top_line, ) ;
+
+while @lines < $!page_size and $current_line < $!foldable.lines  
+	{
+	@lines.push: (@!folds[$current_line][FOLDED] ?? '*' !! ' ') ~ $!foldable.lines[$current_line] ;
+
+	$current_line += @!folds[$current_line][FOLDED] 
+				?? @!folds[$current_line][SKIP] + 1
+				!! 1 ;
+	}
+
+@lines
+}
+
+method search() {}
+
+} # class
+
+
---
+++ t/90_foldable.t
@@ -0,0 +1,134 @@
+#!/usr/bin/env perl6
+
+use Data::Dump::Tree ;
+use Data::Dump::Tree::Foldable ;
+
+use Test ;
+plan 26 ;
+
+# DDT filter to show the folding internal data in a better way 
+sub filter(\r, $s, ($, $path, $glyph, @renderings), (\k, \b, \v, \f, \final, \want_address))
+{
+r = Data::Dump::Tree::Type::Nothing if k ~~ /'$.foldable'/ ;
+
+if k ~~ /'@.folds'/ 
+	{
+	try 
+		{
+		require Text::Table::Simple <&lol2table> ;
+
+		r = lol2table(
+			< index skip folded parent >,
+			($s.List Z 0..*).map: -> ($d, $i) { [$i, |$d] },
+			headers => corner_marker => '+'
+			).join("\n").List ;
+
+		}
+
+	@renderings.push: "$!" if $! ;
+	}
+}
+
+my $f = Data::Dump::Tree::Foldable.new: (^20).List, :title<title>, :!color ;
+my $g = $f.get_view ;
+	my @dump = $g.get_lines ;
+	is @dump.elems, 21, '21 lines' or diag @dump.join("\n") ;
+	like @dump[0], /title/, 'top is title' or diag @dump.join("\n") ;
+
+
+$g.set: :top_line<1>, :page_size<10> ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 10, '10 lines' or diag @dump.join("\n") ;
+	like @dump[0], /0/, 'top line changed' or diag @dump.join("\n") ;
+ 
+$g.set: :page_size<-10> ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 0, '0 lines' or diag @dump.join("\n") ;
+
+$g.set: :page_size<10> ;
+$g.line_down ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 10, '10 lines' or diag @dump.join("\n") ;
+	like @dump[0], /1/, 'line down' or diag @dump.join("\n") ;
+
+$g.line_down for ^20;
+	@dump = $g.get_lines ;
+	is @dump.elems, 1, '1 line' or diag @dump.join("\n") ;
+	like @dump[0], /19/, 'last line' or diag @dump.join("\n") ;
+
+$g.page_down ;
+	#diag get_dump $g, header_filters => (&filter,) ;
+	@dump = $g.get_lines ;
+	like @dump[0], /19/, 'page down, last line' or diag @dump.join("\n") ;
+
+$g.page_up ;
+	@dump = $g.get_lines ;
+	like @dump[0], /9/, 'page up' or diag @dump.join("\n") ;
+
+$g.page_up for ^15;
+	@dump = $g.get_lines ;
+	like @dump[0], /title/, 'max page up' or diag @dump.join("\n") ;
+
+$g.line_up ;
+	@dump = $g.get_lines ;
+	like @dump[0], /title/, 'max line up' or diag @dump.join("\n") ;
+
+
+class Tomatoe{ has $.color ;}
+my $s =
+        [
+        Tomatoe,
+        [ [ [ Tomatoe, ] ], ],
+        123,
+        Tomatoe.new( color => 'green'),
+	(^5).list,
+        ] ;
+
+$f = Data::Dump::Tree::Foldable.new($s, :title<title>, :!color) ;
+$g = $f.get_view ;
+	#diag get_dump ($f => $g) ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 15, '15 lines' or diag @dump.join("\n") ;
+
+$g.fold_flip_selected ;
+	#diag get_dump $g, header_filters => (&filter,) ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 1, '1 line fold' or diag @dump.join("\n") ;
+
+$g.fold_all ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 1, '1 line fold' or diag @dump.join("\n") ;
+
+$g.unfold_all ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 15, '15 lines' or diag @dump.join("\n") ;
+
+$g.set: :selected_line(3) ;
+$g.fold_flip_selected ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 13, '13 lines, 1 fold' or diag @dump.join("\n") ;
+
+$g.set: :selected_line(7) ;
+$g.fold_flip_selected ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 8, '8 lines, 2 folds' or diag @dump.join("\n") ;
+
+$g.line_down for ^3 ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 5, '5 lines, 2 folds' or diag @dump.join("\n") ;
+	like @dump[0], /1/, 'at index 1' or diag @dump.join("\n") ;
+
+$g.line_down  ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 4, '4 lines, 2 folds' or diag @dump.join("\n") ;
+	like @dump[0], /2/, 'at index 2' or diag @dump.join("\n") ;
+
+$g.line_up ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 5, '5 lines, 2 folds' or diag @dump.join("\n") ;
+	like @dump[0], /1/, 'at index 1' or diag @dump.join("\n") ;
+
+$g.line_up for ^3 ;
+	@dump = $g.get_lines ;
+	is @dump.elems, 8, '8 lines, 2 folds' or diag @dump.join("\n") ;
+
